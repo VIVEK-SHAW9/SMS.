9512fab5781980b8cd540dd4e1042282bc97b950
Vivek Shaw <vivekshaw829@gmail.com>
2025-08-01 11:51:11 +0530

    project

---
+++ Frontend/src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ function Banner() {
+  return (
+   <>
+   <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+    <div className='w-full md:w-1/2'>
+   <h1 className="text-4xl">
+     welcome to JIS chool of Management.Top school of the west bengal
+      </h1>
+    </div>
+    
+    <div className='w-full md:w-1/2'>right</div>
+   </div>
+   </>
+  );
+}
+export default Banner;
